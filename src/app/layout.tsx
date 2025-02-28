45fbd57d77d708823dd27eb6f1a05cfe4398cd0b
joaovitornara10 <54191082+joaovitornara10@users.noreply.github.com>
2025-02-28 10:01:07 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+ <main className=" h-full pt-10 bg-bannerImg bg-repeat bg-cover bg-bottom w-full " >
         {children}
+      </main>     
       </body>
     </html>
   );
