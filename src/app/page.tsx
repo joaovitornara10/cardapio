a5e0843c79271613c603fdd99589e04144751080
joaovitornara10 <54191082+joaovitornara10@users.noreply.github.com>
2025-03-06 20:21:35 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="flex flex-col ">
 
             <div className="text-gray-100  w-full p-2 text-center justify-start items-center">
-                <h1 className=" text-3xl"> BRASA BURGER </h1>
+                <h1 className=" text-3xl"> BRASA BURGUER </h1>
                 <span className=" text-xl ">A ROTA DO SABOR</span>
             </div>
 
