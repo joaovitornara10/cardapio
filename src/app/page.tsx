bce0090ad612d72bfe5e10292259c9af989709a6
joaovitornara10 <54191082+joaovitornara10@users.noreply.github.com>
2025-03-07 17:16:00 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,9 +23,6 @@ export default function Home() {
                 </div>
             </div>
 
-
-{/*
-
             <div className=" flex flex-col gap-2 border-2 border-orange-500 m-1.5 p-1.5 ">
                 <div className="p-2 w-full justify-center text-center text-xl  ">
                     <h2 className=" text-orange-500">OFERTA DO DIA </h2>
@@ -40,7 +37,6 @@ export default function Home() {
                 </div>
             </div>
 
-*/}
 
             {
                 destaque.map((items, index) => (
