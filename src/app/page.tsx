2aeac623ea82689aa1eb79f0e3281682f32eeec7
joaovitornara10 <54191082+joaovitornara10@users.noreply.github.com>
2025-03-12 18:31:12 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ export default function Home() {
 
     return (
         <div className="flex flex-col ">
-
+            {/*
             <div className="text-gray-100  w-full p-2 text-center justify-start items-center">
                 <h1 className=" text-3xl"> BRASA BURGUER </h1>
                 <span className=" text-xl ">A ROTA DO SABOR</span>
             </div>
-
+*/}
             <div className=" flex flex-col gap-2 border-2 border-orange-500 m-1.5 p-1.5 ">
 
                 <div className=" relative w-full h-full ">
