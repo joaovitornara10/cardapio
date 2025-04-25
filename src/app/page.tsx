887ebcf81a6769aedcdfa32851a71a28ae2a1420
joaovitornara10 <54191082+joaovitornara10@users.noreply.github.com>
2025-04-25 18:38:46 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,27 +16,26 @@ export default function Home() {
                 <span className=" text-xl ">A ROTA DO SABOR</span>
             </div>
 
-            <div className=" flex flex-col gap-2 border-2 border-orange-500 m-1.5 p-1.5 ">
+           */}
+             <div className=" flex flex-col gap-2 border-2 border-orange-500 m-1.5 p-1.5 ">
 
                 <div className=" relative w-full h-full ">
-                    <Image src='/bannershol.jpeg'
+                    <Image src='/WhatsApp Image 2025-04-25 at 6.16.43 PM.jpeg'
                         width={600}
                         height={700}
                         alt="destaque" />
                 </div>
             </div>
+            
+             <div className=" flex flex-col gap-2 border-2 border-orange-500 m-1.5 p-1.5 ">
 
-
-                <div className=" flex flex-col gap-2 border-2 border-orange-500 m-1.5 p-1.5 ">
-
-                    <div className=" relative w-full h-full ">
-                        <Image src='/IMG-20250417-WA0070(1).jpg'
-                            width={600}
-                            height={700}
-                            alt="destaque" />
-                    </div>
+                <div className=" relative w-full h-full ">
+                    <Image src='/WhatsApp Image 2025-04-25 at 6.16.34 PM.jpeg'
+                        width={600}
+                        height={700}
+                        alt="destaque" />
                 </div>
-                */}  
+            </div>
                 <div className=" flex flex-col gap-2 border-2 border-orange-500 m-1.5 p-1.5 ">
                 <Link href={"https://cupons.ambev.com.br/chopp251/lets-start"}>
                     <div className=" relative w-full h-full ">
