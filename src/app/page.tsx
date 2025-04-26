24d47b1f5f3384c09f0f69dd4d06e31db712bde4
joaovitornara10 <54191082+joaovitornara10@users.noreply.github.com>
2025-04-26 18:28:58 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
                 <span className=" text-xl ">A ROTA DO SABOR</span>
             </div>
 
-           */}
+          
              <div className=" flex flex-col gap-2 border-2 border-orange-500 m-1.5 p-1.5 ">
 
                 <div className=" relative w-full h-full ">
@@ -46,7 +46,7 @@ export default function Home() {
                     </div>
                 </Link>                    
                 </div>
-*/}
+
 
                 <div className=" flex flex-col gap-2 border-2 border-orange-500 m-1.5 p-1.5 ">
                     <div className="p-2 w-full justify-center text-center text-xl  ">
@@ -62,7 +62,7 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-
+*/}
             {
                 destaque.map((items, index) => (
                     <div key={index} className=" flex flex-col gap-2 border-2 border-orange-500 m-1.5 p-1.5 ">
