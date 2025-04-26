4e0a08bdbc19b67eea3b33a9f6a59379cc9954d8
joaovitornara10 <54191082+joaovitornara10@users.noreply.github.com>
2025-04-26 19:27:14 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,15 @@ export default function Home() {
 
     return (
         <div className="flex flex-col ">
-          
+           <div className=" flex flex-col gap-2 border-2 border-orange-500 m-1.5 p-1.5 ">
+
+                <div className=" relative w-full h-full ">
+                    <Image src='/WhatsApp Image 2025-04-25 at 6.16.34 PM.jpeg'
+                        width={600}
+                        height={700}
+                        alt="destaque" />
+                </div>
+            </div>
             {
                 destaque.map((items, index) => (
                     <div key={index} className=" flex flex-col gap-2 border-2 border-orange-500 m-1.5 p-1.5 ">
