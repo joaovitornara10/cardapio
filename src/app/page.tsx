3b984e24d1b54db404abc3fbb14afbcff326fca9
joaovitornara10 <54191082+joaovitornara10@users.noreply.github.com>
2025-06-18 20:12:38 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
              <div className=" flex flex-col gap-2 border-2 border-orange-500 m-1.5 p-1.5 ">
 
                 <div className=" relative w-full h-full ">
-                    <Image src='/IMG-20250417-WA0070(1).jpg'
+                    <Image src='/WhatsApp Image 2025-06-18 at 8.07.16 PM.jpeg'
                         width={600}
                         height={700}
                         alt="destaque" />
