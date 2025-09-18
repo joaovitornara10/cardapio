46ad4b8054aa6d5a82fdfd357cf73656fc9f2558
joaovitornara10 <54191082+joaovitornara10@users.noreply.github.com>
2025-09-18 18:17:14 -0400

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1078,7 +1078,7 @@ export const  combos = ([
             {
                 id: "531",
                 name: "COMBO DE FRITAS",
-                price: "93",
+                price: "92,99",
                 category: "lanche",
                 description:"Batata, Calabresa, Mandioca e Filé de Frango"
             },
@@ -1107,6 +1107,7 @@ export const  combos = ([
         ]
     }
 ]);
+
 
 
 
